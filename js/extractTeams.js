93fa1f2bd51d59bf6c4aa50de366689f9d5b91f7
Hanping Zhong <kgbruce@umich.edu>
2020-10-30 22:45:23 +0800

    a little styling using bootstrap, added reddit's links, redirection not working

--- js/extractTeams.js
+++ js/extractTeams.js
@@ -1,4 +1,4 @@
-import { TEAMS } from './teams.js'
+import { TEAMS } from "./teams.js";
 
 /* Function that extracts team names based on given inputs
  * title: String. REQUIRED.     Title of the ESPN page
@@ -6,45 +6,47 @@ import { TEAMS } from './teams.js'
  * keywords: String. REQUIRED.  Keywords meta of the ESPN Page
  * content: String              Content of the ESPN page
  */
-export function extractTeams(title, url, keywords = '', content = '') {
-    // get leauge from URL
-    const url_paths = url.split('/');
-    const league = Object.keys(TEAMS).find(league => url_paths.includes(league.toLocaleLowerCase()));
-    console.log('league: ' + league);
-    console.log('title: ' + title);
-    console.log('keywords: ' + keywords);
+export function extractTeams(title, url, keywords = "", content = "") {
+  // get leauge from URL
+  const url_paths = url.split("/");
+  const league = Object.keys(TEAMS).find((league) =>
+    url_paths.includes(league.toLocaleLowerCase())
+  );
+  console.log("league: " + league);
+  console.log("title: " + title);
+  console.log("keywords: " + keywords);
 
-    let all_teams = []
-    if (league) {
-        all_teams = TEAMS[league];
-    } else {
-        Object.keys(TEAMS).forEach(league => {
-            all_teams.concat(TEAMS[league]);
-        })
-    }
-
-    // extract team names from keywords
-    let teams_from_keywords = []
-    if (keywords) {
-        // keywords format: "kw1, kw2, kw3, kw4"
-        const kw_list = keywords.split(',').map(kw => {
-            return kw.trim();
-        });
-
-        teams_from_keywords = all_teams.filter(team => {
-            return kw_list.includes(team.fullname);
-        })
-    }
-    console.log('teams_from_keywords' + teams_from_keywords);
-
-    // extract team names from title
-    let teams_from_title = all_teams.filter(team => {
-        const names = [team.fullname, team.team];
-        names.concat(team.other);
-
-        return names.some(w => title.includes(w));
+  let all_teams = [];
+  if (league) {
+    all_teams = TEAMS[league];
+  } else {
+    Object.keys(TEAMS).forEach((league) => {
+      all_teams.concat(TEAMS[league]);
     });
-    console.log('teams_from_title' + teams_from_title);
+  }
 
-    return [...new Set([...teams_from_keywords, ...teams_from_title])];
+  // extract team names from keywords
+  let teams_from_keywords = [];
+  if (keywords) {
+    // keywords format: "kw1, kw2, kw3, kw4"
+    const kw_list = keywords.split(",").map((kw) => {
+      return kw.trim();
+    });
+
+    teams_from_keywords = all_teams.filter((team) => {
+      return kw_list.includes(team.fullname);
+    });
+  }
+  console.log("teams_from_keywords" + teams_from_keywords);
+
+  // extract team names from title
+  let teams_from_title = all_teams.filter((team) => {
+    const names = [team.fullname, team.team];
+    names.concat(team.other);
+
+    return names.some((w) => title.includes(w));
+  });
+  console.log("teams_from_title" + teams_from_title);
+
+  return [...new Set([...teams_from_keywords, ...teams_from_title])];
 }
--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-import { extractTeams } from './extractTeams.js'
+import { extractTeams } from "./extractTeams.js";
 
 /* Get the keywords meta of the active ESPN page, and then send a message that includes the meta content,
  * page url, and page title.
@@ -6,13 +6,13 @@ import { extractTeams } from './extractTeams.js'
  * keyword meta example: <meta name="keywords", content="NBA, stats, Los Angeles Lakers, Miami Heat" />
  */
 chrome.tabs.query(
-    {
-        active: true,
-        currentWindow: true
-    }, 
-    function(tabs) {
-        // Code that will be ran by the active ESPN page to get keywords meta and send the message
-        const code = `
+  {
+    active: true,
+    currentWindow: true,
+  },
+  function (tabs) {
+    // Code that will be ran by the active ESPN page to get keywords meta and send the message
+    const code = `
         var metas = document.getElementsByTagName('meta'); 
         var keywords_meta = '';
 
@@ -31,58 +31,72 @@ chrome.tabs.query(
             tab_title: "${tabs[0].title}"
         });`;
 
-        chrome.tabs.executeScript(tabs[0].id, {
-            code: code
-        }, function() {
-            if (chrome.runtime.lastError) {
-                // Failed to get meta tags of ESPN page
-                console.log('There was an error : \n' + chrome.runtime.lastError.message);
-                
-                const teams = extractTeams(tabs[0].title, tabs[0].url);
-                console.log('Teams found: ');
-                teams.forEach(element => {
-                    console.log(element)
-                });
-            }
-        });
-    }
+    chrome.tabs.executeScript(
+      tabs[0].id,
+      {
+        code: code,
+      },
+      function () {
+        if (chrome.runtime.lastError) {
+          // Failed to get meta tags of ESPN page
+          console.log(
+            "There was an error : \n" + chrome.runtime.lastError.message
+          );
+
+          const teams = extractTeams(tabs[0].title, tabs[0].url);
+          console.log("Teams found: ");
+          teams.forEach((element) => {
+            console.log(element);
+          });
+        }
+      }
+    );
+  }
 );
 
-/* Listener for messages sent by active ESPN page. Upon receiving the message, the listener should 
+/* Listener for messages sent by active ESPN page. Upon receiving the message, the listener should
  * run the 'main' function that provides users with links to discussion threads.
  */
-chrome.runtime.onMessage.addListener(function(request, sender) {
-    if (request.method == "getKeywordsMeta") {
-        console.log('Metas in listener: ' + request.metas);
+chrome.runtime.onMessage.addListener(function (request, sender) {
+  if (request.method == "getKeywordsMeta") {
+    console.log("Metas in listener: " + request.metas);
 
-        // First extract teams
-        const teams = extractTeams(request.tab_title, request.tab_url, request.metas);
+    // First extract teams
+    const teams = extractTeams(
+      request.tab_title,
+      request.tab_url,
+      request.metas
+    );
 
-        console.log('Teams found: ');
-        const all_teams_div = document.getElementById('teams');
+    console.log("Teams found: ");
+    const all_teams_div = document.getElementById("teams");
 
-        teams.forEach(element => {
-            console.log(element);
-            const team_div = document.createElement('div');
-            team_div.setAttribute('class', 'team');
-            team_div.setAttribute('id', element.fullname);
-            
-            const title = document.createElement('h3');
-            title.innerHTML = element.fullname;
+    teams.forEach((element) => {
+      console.log(element);
+      const team_div = document.createElement("div");
+      team_div.setAttribute("class", "team");
+      team_div.setAttribute("id", element.fullname);
 
-            const reddit_link = document.createElement('p');
-            reddit_link.innerHTML = element.reddit;
-            reddit_link.setAttribute('class', 'reddit');
+      const title = document.createElement("h3");
+      title.innerHTML = element.fullname;
 
-            const discord_link = document.createElement('p');
-            discord_link.innerHTML = element.discord;
-            discord_link.setAttribute('class', 'discord');
+      //   const reddit_link = document.createElement("p");
+      //   reddit_link.innerHTML = element.reddit;
+      //   reddit_link.setAttribute("class", "reddit badge badge-primary");
+      const reddit_link = document.createElement("a");
+      let linktext = document.createTextNode(element.fullname);
+      reddit_link.appendChild(linktext);
+      reddit_link.href = element.reddit;
+      reddit_link.setAttribute("class", "reddit badge badge-primary");
 
-            team_div.appendChild(title);
-            team_div.appendChild(reddit_link);
-            team_div.appendChild(discord_link);
-            all_teams_div.appendChild(team_div);
-        });
-    }
+      const discord_link = document.createElement("p");
+      discord_link.innerHTML = element.discord;
+      discord_link.setAttribute("class", "discord badge badge-warning");
+
+      team_div.appendChild(title);
+      team_div.appendChild(reddit_link);
+      team_div.appendChild(discord_link);
+      all_teams_div.appendChild(team_div);
+    });
+  }
 });
-
